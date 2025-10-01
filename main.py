0ff2963b9742608867038b1e72f10f864f30d70f
Kitsori <owenreds19@gmail.com>
2025-10-01 16:31:48 -0500

    Fixing Embed Order

--- main.py
+++ main.py
@@ -150,13 +150,12 @@ async def girlranking(ctx):
         name, path = randomGirlGen()  # Make a tuple of the name of image and its filepath
 
         filename = os.path.basename(path)
-
         file = discord.File(path, filename=filename)  # Prepare the actual image
 
         embed = discord.Embed(title=name, color=discord.Color.blue()) # Set embed left side color
         embed.set_image(url=f"attachment://{filename}") # Set the image?
 
-        await ctx.send(embed=embed, file=file) # Send the embed of name and girl image
+        await ctx.send(file=file, embed=embed) # Send the embed of name and girl image
         await asyncio.sleep(2)
 
 
